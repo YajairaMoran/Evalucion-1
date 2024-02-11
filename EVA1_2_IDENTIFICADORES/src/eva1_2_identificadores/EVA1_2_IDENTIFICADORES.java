845b75f7d64f75f28a719b1cacd276c75c1cbb73
YajairaMoran <159494447+YajairaMoran@users.noreply.github.com>
2024-02-10 20:44:51 -0600

    Add files via upload

---
+++ EVA1_2_IDENTIFICADORES/src/eva1_2_identificadores/EVA1_2_IDENTIFICADORES.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_identificadores;
+
+/**
+ *
+ * @author danie
+ */
+public class EVA1_2_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VARIABLES:
+        //TIPO DE DATO + NOMBRE DEL IDENTIFICADOR:
+        //short int byte long --> enteros
+        //float double --> numeros con decimal
+        //char --> caracteres
+        //boolean --> verdadero o falso
+        //String --> cadenas de texto
+        //si no se declara una variable, no se puede usar
+        int edad; //declaración de una variable
+        String nombre;
+        double salario;
+        
+        //ASIGNACION DE VALORES:
+        //IDENTIFICADOR = VALOR;
+        //se coloca el valor 50 dento de la variable edad
+        edad = 50;
+        nombre = "Juan Perez Jolote";
+        salario = 123.56;
+        //COMO LOS USAMOS:
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
+//MATRICULA:24550022
+//NOMBRE: Yajaira Daniela Moran Baca
+//Grupo:B
