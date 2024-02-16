b2053eb00602f7a76fbab0eec4ed90cca4e132b4
YajairaMoran <159494447+YajairaMoran@users.noreply.github.com>
2024-02-15 22:30:12 -0600

    Add files via upload

---
+++ EVA1_4_ASIGNACION/src/eva1_4_asignacion/EVA1_4_ASIGNACION.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_asignacion;
+
+/**
+ *
+ * @author danie
+ */
+public class EVA1_4_ASIGNACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre;
+        nombre = "Yajaira Moran";
+        int edad;//DECLARACIÓN
+        edad = 21; //ASIGNACIÓN--> UN SOLO = SIGNIFICA ASIGNAR
+        String carrera;
+        carrera = "Ingenieria en sistemas computacionales";
+        System.out.println("Tu nombre es:");
+        System.out.println(nombre);
+        System.out.println("Tu edad es:");
+        System.out.println(edad);
+        System.out.println("La carrera que cursas es:");
+        System.out.println(carrera);
+    }
+    
+}
